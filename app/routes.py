0ba35410f2195eefdccfb8733876b582d3c45be7
shnider42 <holtsniderc@gmail.com>
2026-05-05 01:27:59 -0400

    Add production utility routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Response, render_template
 
 main = Blueprint("main", __name__)
 
@@ -12,10 +12,33 @@ def home():
     )
 
 
-@main.route("/proof")
-def proof():
-    return render_template(
-        "proof.html",
-        business_name="Holtsnider Tech",
-        tagline="Proof of work and technical depth.",
-    )
+@main.route("/healthz")
+def healthz():
+    return {"status": "ok", "service": "holtsnidertech"}
+
+
+@main.route("/robots.txt")
+def robots_txt():
+    body = "User-agent: *\nAllow: /\nSitemap: https://holtsnidertech.com/sitemap.xml\n"
+    return Response(body, mimetype="text/plain")
+
+
+@main.route("/sitemap.xml")
+def sitemap_xml():
+    body = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://holtsnidertech.com/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://holtsnidertech.com/static/demos/grepper.html</loc>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://holtsnidertech.com/static/demos/loudsource-vote.html</loc>
+    <priority>0.7</priority>
+  </url>
+</urlset>
+"""
+    return Response(body, mimetype="application/xml")
